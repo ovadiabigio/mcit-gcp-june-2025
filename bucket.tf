109d82779306ae844af7e05a60624dc2985cea46
ovadiabigio <145715771+ovadiabigio@users.noreply.github.com>
2025-06-13 18:19:18 -0400

    Create bucket.tf

---
+++ bucket.tf
@@ -0,0 +1,8 @@
+module "my_bucket" {
+  source        = "./modules/gcs_bucket"
+  bucket_name   = var.bucket_name
+  location      = var.region
+  storage_class = var.storage_class
+  versioning    = var.versioning
+  environment   = var.environment
+}

cd4ae6c43417d5132d48c3d0a613b10284927bf5
ovadiabigio <145715771+ovadiabigio@users.noreply.github.com>
2025-06-13 18:17:32 -0400

    Create main.tf

---
+++ modules/gcs-bucket/main.tf
@@ -0,0 +1,24 @@
+resource "google_storage_bucket" "this" {
+  name          = var.bucket_name
+  location      = var.location
+  storage_class = var.storage_class
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+
+  labels = {
+    environment = var.environment
+  }
+}
